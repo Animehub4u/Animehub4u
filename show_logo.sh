25029318b938372d80a71aac338ca2f7d6611d19
Animehub4u <136146660+Animehub4u@users.noreply.github.com>
2024-11-11 00:41:53 +0530

    Update show_logo.sh

--- show_logo.sh
+++ show_logo.sh
@@ -1,1 +1,34 @@
-
+echo -e "\e[1m                                                        
+                      ..                                
+                     .;:.                                 
+                    .;ol,.                                
+                   .;ooc:'                                
+            ..    .;ooccc:'.    ..                        
+          .',....'cdxlccccc;.....,'.                      
+         .;;..'';clolccccccc:,''..;;.                     
+        ':c'..':cccccccccccccc;...'c:.                    
+       ':cc,.'ccccccccccccccccc:..;cc:'                   
+    ...:cc;.':cccccccccccccccccc:..:cc:...                
+   .;';cc;.':;;:cccccccccccccc:;;;'.;cc,,;.               
+  .cc':c:.',.....;cccccccccc;.....,..:c:'c:               
+  ,x:'cc;.,'     .':cccccc:'.     ',.;cc':x'              
+  lO,'cc;.;,       .;cccc:.       ,;.;cc';0l              
+ .o0;.;c;.,:'......',''''''......':,.;c;.:0l.             
+ .lxl,.;,..;c::::;:,.    .,:;::::c;..,;.,oxl.             
+ .lkxOl..  ..'..;::'..''..'::;..'..  ..c0xkl.             
+  .cKMx.        .;c:;:cc:;:c:.        .xMKc.              
+    ;KX:         ;o::l:;cc;o:.        ;KK;                
+     :KK:.       ,d,cd,'ol'o:       .:0K:                 
+      ;0NOl:;:loo;. ... .. .;ldlc::lkN0:                  
+       .lONNNKOx0Xd,;;'.,:,lKKkk0XNN0o.                   
+         .','.. .lX0doooodOXd.  .','.                     
+                 .,okkddxkd;.                             
+                    'oxxd;.                               
+   ...................................................                             
+   .   A i r d r o p   S c r i p t   Z o n e  .  
+   ...................................................                
+                                                              
+\e[0m"
+echo -e "\e[1mSkel Airdrop Script Zone\e[0m";
+echo -e "\e[1mSubscribe Telegram Channel: \e[4;35mhttps://t.me/airdropscriptzone\e[0m";
+echo -e ""
